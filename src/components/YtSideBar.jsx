7396fef312d2c0c9f6d47c6ba4b3bbd61c2a3ec3
usmanhasann <osmanmuhammadofficial@gmail.com>
2024-11-24 23:50:48 +0500

    Added favicons

--- src/components/YtSideBar.jsx
+++ src/components/YtSideBar.jsx
@@ -1,7 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { SiYoutubemusic } from "react-icons/si";
+import { FaUserCircle } from "react-icons/fa";
+import { LiaDownloadSolid } from "react-icons/lia";
+
 const SideBar = () => {
   return (
     <>
-      <div className="fixed left-0 bottom-0  "></div>
+      <div className="fixed left-0  z-[2028] p-[0 4px] inline-block">
+        <div className="flex flex-col mt-[4px]">
+          <FontAwesomeIcon icon={faHouse} />
+          <SiYoutubeshorts />
+          <MdOutlineSubscriptions />
+          <SiYoutubemusic />
+          <FaUserCircle />
+          <LiaDownloadSolid />
+        </div>
+      </div>
     </>
   );
 };
